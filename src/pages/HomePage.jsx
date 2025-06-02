1f1002fe0cef3add0402a0ea09d033b6f8217b91
simar <gursimar.chauhan@gmail.com>
2025-06-02 13:13:37 +0530

    xyz

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LeftPanel from '../Components/Home/LeftPanel'
 import MiddlePanel from '../Components/Home/MiddlePanel'
 import RightPanel from '../Components/Home/RightPanel'
 
 const HomePage = () => {
+    const [selectedUser, setSelectedUser] = useState(false)
     return (
-        <>
-            <LeftPanel />
-            <MiddlePanel />
-            <RightPanel />
-        </>
+        <div>
+            <div className=''>
+                <LeftPanel />
+                <MiddlePanel />
+                <RightPanel />
+            </div> 
+        </div>
     )
 }
-
 export default HomePage
