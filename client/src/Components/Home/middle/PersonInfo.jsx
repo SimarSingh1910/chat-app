c11ad0599b63ef8e4a3bb1f0ce106e50505b1eda
SimarSingh1910 <147783127+SimarSingh1910@users.noreply.github.com>
2025-07-29 14:08:50 +0530

    responsive

--- client/src/Components/Home/middle/PersonInfo.jsx
+++ client/src/Components/Home/middle/PersonInfo.jsx
@@ -8,11 +8,13 @@ const PersonInfo = ({ selectedUser }) => {
   console.log(selectedUser.first_name)
   return (
     <div>
-      <div className='flex items-center gap-3 py-3 mx-4 border-b-2 border-stone-200'>
+      <div className='flex items-center border justify-between py-3 mx-4 border-b-2 border-stone-200'>
+        <div className='flex space-around border items-center gap-3 w-50'>
         <img src={demoDP} alt="display pic" className='w-15 rounded-full object-cover h-15'/>
-        <p>{`${selectedUser.first_name} ${selectedUser.last_name}`}</p>
+        <p className='border'>{`${selectedUser.first_name} ${selectedUser.last_name}`}</p>
         <span className='rounded-full w-3 h-3 bg-green-600'></span>
-        <div className='flex items-center ml-70 gap-3'>
+        </div>
+        <div className='flex items-center justify-evenly border w-20'>
           <img src={search} alt="" className='relative w-9 h-9'/>
           <img src={phone} alt="" className='relative w-9 h-9'/>
         </div>
